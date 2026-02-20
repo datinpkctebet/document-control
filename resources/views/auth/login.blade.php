32ab56c073f83eeedb7710aa5107cd0aa06f8ed6
Muara Firdaus <muarafirdaus713@gmail.com>
2026-02-20 17:10:36 +0700

    Update Logo dan Tampilan Login v2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
             
             <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
                 <a href="#" class="mb-12">
-                    <img alt="Logo" src="{{ asset('assets/media/logos/logotebethitam.png') }}" class="h-70px" />
+                    <img alt="Logo" src="{{ asset('assets/media/logos/logotebethitam.png') }}" class="h-70px"/>
                 </a>
                 
                 <div class="w-lg-500px bg-body rounded shadow-sm p-10 p-lg-15 mx-auto">
